dd8f8c1771a57b1a8bda1af1c044d2692c120539
alibabayev2002 <acemiyt23@gmail.com>
2020-09-15 10:28:16 +0400

    stop

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,15 +2,22 @@
   <div>
       <div class="game-content">
         <div>
-          <button @click="append(1)" id="1" :class="{'fade' : one}" style="" class="green">yesil</button>
-          <button @click="append(2)" id="2" :class="{'fade' : two}" style="" class="blue">mavi</button>
+          <button @click="append(1)" id="1" :class="{'fade' : one}" style="opacity:.3" class="green">yesil</button>
+          <button @click="append(2)" id="2" :class="{'fade' : two}" style="opacity:.3" class="blue">mavi</button>
         </div>
         <div>
-          <button @click="append(3)" id="3" :class="{'fade' : three } " style="" class="red">kirmizi</button>
-          <button @click="append(4)" id="4" :class="{'fade' : four}" class="yellow">sari</button>
+          <button @click="append(3)" id="3" :class="{'fade' : three } " style="opacity:.3" class="red">kirmizi</button>
+          <button @click="append(4)" id="4" :class="{'fade' : four}" style="opacity:.3" class="yellow">sari</button>
         </div>
-      </div>
-    <br><br>
+        
+      </div><br>
+      <input v-model="difficulty" checked type="radio" id="male" name="gender" checked value="easy">
+        <label style="margin-right:10px" for="easy"> easy</label>
+        <input v-model="difficulty" type="radio" id="female" name="gender" value="normal">
+        <label for="female"> normal </label>
+        <input v-model="difficulty" type="radio" id="cetin" name="gender" value="cetin">
+        <label style="margin-right:10px" for="cetin"> cetin</label>
+    <br>
     <button @click="start()" class="submit">GO</button><br> <br>
     <b>Round : </b>{{this.count}} <br>
     <b>Rekor round: </b> {{$cookies.get('rekor') ? $cookies.get('rekor') : '1'}}
@@ -32,6 +39,8 @@
         two: false,
         three: false,
         four: false,
+        betweentime : 400,
+        difficulty:"easy",
       }
     },
     methods: {
@@ -40,33 +49,40 @@
       },
       start() {
         var num
-        var z = 1
         var time = 0
-        var animateTime = 500
+        if(this.difficulty == "easy"){
+          this.betweentime = 1500
+        }
+        if(this.difficulty == "normal"){
+          this.betweentime = 1000
+        }
+         if(this.difficulty == "cetin"){
+          this.betweentime = 400
+        }
         if (this.confirmedTest()) {
           for (var j = 0; j < this.count; j++) {
-            time = time + animateTime * z
+            time = time + this.betweentime
             num = this.getRandomInt(1, 4)
             this.confirm.push(num)
-            if (num == 1) {
-              setTimeout(() => this.one = true, time)
-              setTimeout(() => this.one = false, time + animateTime)
-              z += 1
+             if (num == 1) {
+              setTimeout(() => this.one = true, time);
+              setTimeout(() => this.one = false, time+this.betweentime-(this.betweentime/10));
+              continue
             }
-            if (num == 2) {
-              setTimeout(() => this.two = true, time)
-              setTimeout(() => this.two = false, time + animateTime)
-              z += 1
+             if (num == 2) {
+              setTimeout(() => this.two = true, time);
+              setTimeout(() => this.two = false, time+this.betweentime-(this.betweentime/10));
+              continue
             }
-            if (num == 3) {
-              setTimeout(() => this.three = true, time)
-              setTimeout(() => this.three = false, time + animateTime)
-              z += 1
+             if (num == 3) {
+              setTimeout(() => this.three = true, time);
+              setTimeout(() => this.three = false, time+this.betweentime-(this.betweentime/10));
+              continue
             }
-            if (num == 4) {
-              setTimeout(() => this.four = true, time)
-              setTimeout(() => this.four = false, time + animateTime)
-              z += 1
+             if (num == 4) {
+              setTimeout(() => this.four = true, time);
+              setTimeout(() => this.four = false, time+this.betweentime-(this.betweentime/10));
+              continue
             }
           }
         } else {
@@ -74,7 +90,6 @@
           this.array = []
           this.confirm = []
         }
-
 
 
 
@@ -97,36 +112,34 @@
         }
         return bool
 
-      }
-
-
-
-
+      },
     },
     watch: {
       array() {
-
         if (this.array.length == this.count) {
           if (this.confirmedTest()) {
             this.count++
             this.array = []
             this.confirm = []
             this.start()
-            Vue.$cookies.set('rekor', this.count)
+
           } else {
-            this.count = 1
             this.array = []
             this.confirm = []
+            if($cookies.get('rekor') && parseInt($cookies.get('rekor')) < this.count){
+              Vue.$cookies.set('rekor', this.count)
+            }
+            console.log($cookies.get('rekor'))
+            this.count = 1
           }
         }
       }
-    },
-  }
+    }
+    }
+
 </script>
 <style scoped>
   /* Buttons style */
- 
-
   .green {
     border-top-left-radius: 105px;
     background-color: green;
@@ -193,12 +206,6 @@
     width: 45%;
     opacity: 0.3;
     border: white 1px solid;
-
-    transition: opacity 0.3 ease-in-out;
-    -webkit-transition: opacity 0.4s ease-in-out;
-    -moz-transition: opacity 0.4s ease-in-out;
-    -ms-transition: opacity 0.4s ease-in-out;
-    -o-transition: opacity 0.4s ease-in-out;
   }
 
   .green,
@@ -210,12 +217,12 @@
   }
 
   .fade {
-    animation: fade .5s linear;
+    animation: fade .4s linear;
   }
    @keyframes fade {
 
     0%{
-      opacity: .3;
+      opacity: .3
     }
     50% {
       opacity: 1
